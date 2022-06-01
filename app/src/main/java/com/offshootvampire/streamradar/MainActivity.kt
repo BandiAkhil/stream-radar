84f83f7a44a680bd9d27a8457fce27d0b133caf5
BandiAkhil <akhheel99@gmail.com>
2022-06-01 15:03:17 +0200

    Initial Commit

---
+++ app/src/main/java/com/offshootvampire/streamradar/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.offshootvampire.streamradar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.SearchView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        searchBox.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+//            override fun onQueryTextSubmit(query: String?): Boolean {
+//                return false
+//            }
+//
+//            override fun onQueryTextChange(newText: String?): Boolean {
+//                adapter.filter.filter(newText)
+//                return false
+//            }
+//
+//        })
+    }
+}
